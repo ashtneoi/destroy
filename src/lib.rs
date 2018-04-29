63f463da56ca8e35161ca2953f210f00ea147f73
ashtneoi <ashtneoi@gmail.com>
2018-04-29 00:29:44 -0700

    Add GrammarNode skeleton

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,60 @@
 #[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+mod tests;
+
+mod prelude {
+    pub use {e, c, s, p, q, z, g, n, k, t};
+    pub use GrammarNode;
+}
+
+pub enum GrammarNode {
+    Seq(Vec<GrammarNode>),
+    Choice(Vec<GrammarNode>),
+    Star(Box<GrammarNode>),
+    Plus(Box<GrammarNode>),
+    Opt(Box<GrammarNode>),
+    Pos(Box<GrammarNode>),
+    Neg(Box<GrammarNode>),
+    Name(String, Box<GrammarNode>),
+    Link(String),
+    Text(String),
+}
+
+pub fn e(children: Vec<GrammarNode>) -> GrammarNode {
+    GrammarNode::Seq(children)
+}
+
+pub fn c(children: Vec<GrammarNode>) -> GrammarNode {
+    GrammarNode::Choice(children)
+}
+
+pub fn s(child: GrammarNode) -> GrammarNode {
+    GrammarNode::Star(Box::new(child))
+}
+
+pub fn p(child: GrammarNode) -> GrammarNode {
+    GrammarNode::Plus(Box::new(child))
+}
+
+pub fn q(child: GrammarNode) -> GrammarNode {
+    GrammarNode::Opt(Box::new(child))
+}
+
+pub fn z(child: GrammarNode) -> GrammarNode {
+    GrammarNode::Pos(Box::new(child))
+}
+
+pub fn g(child: GrammarNode) -> GrammarNode {
+    GrammarNode::Neg(Box::new(child))
+}
+
+pub fn n(name: &str, child: GrammarNode) -> GrammarNode {
+    GrammarNode::Name(name.to_string(), Box::new(child))
+}
+
+pub fn k(target: &str) -> GrammarNode {
+    GrammarNode::Link(target.to_string())
+}
+
+pub fn t(text: &str) -> GrammarNode {
+    GrammarNode::Text(text.to_string())
 }
