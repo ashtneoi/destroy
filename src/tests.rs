17674f5f1b1eaa59fc8115a0f1da97de642e8495
ashtneoi <ashtneoi@gmail.com>
2018-05-14 16:37:52 -0700

    Test decimal int grammar

--- src/tests.rs
+++ src/tests.rs
@@ -11,3 +11,38 @@ fn minimal() {
     assert_eq!(st.raw, (0, 3));
     assert!(st.children.is_empty());
 }
+
+#[test]
+fn decimal_integer() {
+    let mut g = e(vec![
+        n("dec_nonzero_digit", c(vec![
+            t("1"),
+            t("2"),
+            t("3"),
+            t("4"),
+            t("5"),
+            t("6"),
+            t("7"),
+            t("8"),
+            t("9"),
+        ])),
+        n("dec_digit", c(vec![
+            t("0"),
+            k("dec_nonzero_digit"),
+        ])),
+        n("dec_int", c(vec![
+            t("0"),
+            e(vec![
+                k("dec_nonzero_digit"),
+                s(k("dec_digit")),
+            ]),
+        ])),
+    ]);
+
+    g.parse("dec_int", "0").unwrap();
+    g.parse("dec_int", "1").unwrap();
+    g.parse("dec_int", "9").unwrap();
+    g.parse("dec_int", "10").unwrap();
+    g.parse("dec_int", "19").unwrap();
+    g.parse("dec_int", "99").unwrap();
+}
