9b01d97c336813d0e426e058d6e29c0953a8ab6f
ashtneoi <ashtneoi@gmail.com>
2018-09-05 21:28:09 -0700

    Minimize EOF test case

--- src/tests/regression.rs
+++ src/tests/regression.rs
@@ -1,5 +1,4 @@
 use constructors::*;
-use GrammarAtom;
 use parse::{
     parse_grammar,
     ParseError,
@@ -99,57 +98,16 @@ fn wrong_prefix_order() {
     ]);
 }
 
-static SIMPLE_GRAMMAR_GRAMMAR_STR: &str = r##"
-    nzdigit = '1'..'9'
-    digit = "0" / nzdigit
-    latin_letter = 'a'..'z' / 'A'..'Z'
-
-    comment = "#" (-"\n" %)*
-
-    wso_part = " " / "\t"
-    ws_part = wso_part / comment? "\n"
-    wso = wso_part*
-    ws = ws_part*
-    pwso = wso_part+
-    pws = ws_part+
-
-    hex_digit = digit / 'a'..'f' / 'A'..'F'
-    hex_uint = "0x" hex_digit+
-
-    str =
-        "\""
-        ("\\" ("n" / "t" / "\\" / "\"") / -"\"" -"\n" %)[cp]*
-        "\""
-    cp =
-        hex_uint[hex]
-        / "'" ("\\" ("n" / "t" / "\\" / "'") / -"'" -"\n" %)[raw] "'"
-    cp_range = cp[from] ".." cp[to]
-    ident_initial = latin_letter / "_" / 0x80..0x10FFFF # TODO
-    ident = ident_initial (ident_initial / digit)* # TODO
-
-    expr = expr_seq[opd] (ws "/" ws expr_seq[opd])*
-    expr_seq = expr_affix[opd] (pws expr_affix[opd] -(wso "="))*
-    expr_affix =
-        ("^" / "-")[pre]*
-        expr_atom[opd]
-        ("*" / "+" / "?" / "[" ident[name] "]")[suf]*
-    expr_atom =
-        "%" / str / cp_range[r] / ident[id] / "(" ws expr[expr] ws ")"
-
-    rule = ident[name] wso "=" ws expr[val]
-    grammar = ws (rule wso comment? "\n" ws)* (rule wso comment?)?
-"##;
-
 #[test]
 fn bad_expected_eof() {
     use GrammarAtom::*;
 
-    let g = parse_grammar(SIMPLE_GRAMMAR_GRAMMAR_STR).unwrap();
+    let g = parse_grammar(r#"x = ("a" "b")?"#).unwrap();
 
-    let e = Parser::parse(&g, "grammar", "a =").unwrap_err();
+    let e = Parser::parse(&g, "x", "a").unwrap_err();
     match e {
         ParseError::MatchFail(_, pos, expected) => {
-            assert_eq!(pos.lin, 3);
+            assert_eq!(pos.lin, 1);
             assert!(!expected.contains(&Text("".to_string())));
         },
         e => panic!(format!("{}", e)),
