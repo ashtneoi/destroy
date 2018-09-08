547532f84f01b39df2aced0cbf37d0c994cfb70f
ashtneoi <ashtneoi@gmail.com>
2018-09-08 14:22:20 -0700

    Bump "large grammar" size

--- src/tests/perf.rs
+++ src/tests/perf.rs
@@ -6,7 +6,7 @@ use test::Bencher;
 #[bench]
 fn bench_large_grammar(b: &mut Bencher) {
     let mut large_grammar = "".to_string();
-    for i in 0..80 {
+    for i in 0..10000 {
         large_grammar.push_str(
             &format!("a{} = 'a'..'z'\n", i)
         );
